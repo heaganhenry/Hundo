556f8b18ed6caf0293bd6882c6c4633b8f55f52c
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2021-10-15 12:19:49 -0400

    Complete project

--- js/site.js
+++ js/site.js
@@ -1,4 +1,51 @@
-// Call Hello World
-function helloWorld() {
-    alert("Hello World");
+// start or controller function
+// get values from the page
+function getValues() {
+    // get values from the page by id
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    // parse into integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+    
+    // input validation
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)){
+        let numbers = generateNumbers(startValue, endValue);
+        displayNumbers(numbers);
+    }
+    else
+        alert("You must enter integers");
+}
+
+// logic function
+// generate numbers from startValue to endValue
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
+    for (let i = startValue; i <= endValue; i++){
+        numbers.push(i);
+    }
+
+    return numbers;
+}
+
+// display or view function
+// display numbers and mark even numbers in bold
+function displayNumbers(numbers) {
+    let templateRows = "";
+
+    for (let i = 0; i < numbers.length; i++) {
+        let className = 'even';
+        let number = numbers[i];
+
+        if(number % 2 == 0)
+            className = 'even';
+        else
+            className = 'odd';
+
+        // this line doesn't render correctly with Prism, see the source
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
 }
